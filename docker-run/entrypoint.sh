b9847c32f00310ec5d957c3dc184ba0f3c16602f
Josef Fröhle <josef.froehle@greenbone.net>
2022-01-13 12:07:41 +0100

    action(docker-run): reformat entrypoint.sh

--- docker-run/entrypoint.sh
+++ docker-run/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-env | grep -v '^#' | xargs > docker-run-action.env
+env | grep -v '^#' | xargs >docker-run-action.env
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+if [[ -n $INPUT_USERNAME ]]; then
+  echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+if [[ -n $INPUT_DOCKER_NETWORK ]]; then
+  INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run --env-file docker-run-action.env --workdir "$INPUT_WORKDIR" -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$RUNNER_WORKSPACE:/github/workspace" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --env-file docker-run-action.env --workdir "$INPUT_WORKDIR" -v "/var/run/docker.sock:/var/run/docker.sock" -v "$RUNNER_WORKSPACE:/github/workspace" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
