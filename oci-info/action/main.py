57f042092faedf6f5ff9349a450f728d840f9a98
Pascal Holthaus <pascal.holthaus@greenbone.net>
2024-02-13 17:29:20 +0100

    Fix: oci init in main.py

--- oci-info/action/main.py
+++ oci-info/action/main.py
@@ -32,34 +32,41 @@ def main() -> int:
         user=arg.user,
         password=arg.password,
     )
-    reg_com = Oci(
-        namespace=arg.compare_namespace,
-        reg_domain=arg.compare_reg_domain,
-        reg_auth_domain=arg.compare_reg_auth_domain,
-        reg_auth_service=arg.compare_reg_auth_service,
-        user=arg.compare_user,
-        password=arg.compare_password,
-    )
 
     if arg.command == "list-tags":
         print(reg.get_tags(arg.repository))
     elif arg.command == "compare-tag-annotation":
+        reg_com = Oci(
+            namespace=arg.compare_namespace,
+            reg_domain=arg.compare_reg_domain,
+            reg_auth_domain=arg.compare_reg_auth_domain,
+            reg_auth_service=arg.compare_reg_auth_service,
+            user=arg.compare_user,
+            password=arg.compare_password,
+        )
+
         ano: dict = reg.get_oci_annotations(
             arg.repository, arg.tag, arg.architecture
         ).dict(by_alias=True)
         ano_com: dict = reg_com.get_oci_annotations(
             arg.compare_repository, arg.tag, arg.architecture
         ).dict(by_alias=True)
+
         if arg.mode == "eq":
             if ano[arg.annotation] == ano_com[arg.annotation]:
                 print("True")
+            else:
+                print("False")
         elif arg.mode == "lt":
             if ano[arg.annotation] < ano_com[arg.annotation]:
                 print("True")
+            else:
+                print("False")
         elif arg.mode == "gt":
             if ano[arg.annotation] > ano_com[arg.annotation]:
                 print("True")
-        print("False")
+            else:
+                print("False")
 
     return 0
 
