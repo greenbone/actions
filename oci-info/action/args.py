50baa25893079251d62454799751ac70d7b05e69
Pascal Holthaus <pascal.holthaus@greenbone.net>
2024-02-14 09:49:53 +0100

    Remove: defaults

--- oci-info/action/args.py
+++ oci-info/action/args.py
@@ -20,7 +20,7 @@ def parse_args(args: Optional[Sequence[str]] = None) -> Namespace:
         args: List of command-line arguments.
 
     Returns:
-        Namespace: Parsed arguments.
+        Parsed arguments.
     """
 
     parser = ArgumentParser(
@@ -31,48 +31,33 @@ def parse_args(args: Optional[Sequence[str]] = None) -> Namespace:
     )
     shtab.add_argument_to(parser)
 
-    parser.add_argument(
-        "--repository", help="Repository name", type=str, required=True
-    )
+    parser.add_argument("--repository", help="Repository name", required=True)
     parser.add_argument(
         "--namespace",
         help="Namespace for the registry",
-        type=str,
         required=True,
     )
     parser.add_argument(
         "--user",
         help="User for the registry login",
-        type=str,
-        required=False,
-        default=None,
     )
     parser.add_argument(
         "--password",
         help="Password for the registry login",
-        type=str,
-        required=False,
-        default=None,
     )
     parser.add_argument(
         "--reg-domain",
         help="Registry domain",
-        type=str,
-        required=False,
         default="ghcr.io",
     )
     parser.add_argument(
         "--reg-auth-domain",
         help="Registry authentication domain",
-        type=str,
-        required=False,
         default="ghcr.io",
     )
     parser.add_argument(
         "--reg-auth-service",
         help="Registry authentication service",
-        type=str,
-        required=False,
         default="ghcr.io",
     )
 
@@ -88,76 +73,57 @@ def parse_args(args: Optional[Sequence[str]] = None) -> Namespace:
         help="Compare repository annotations in different registries",
     )
     compare_annotations_parser.add_argument(
-        "--tag", help="Tag to compare", type=str, required=True
+        "--tag", help="Tag to compare", required=True
     )
     compare_annotations_parser.add_argument(
         "--architecture",
         help="Annotation from architecture to compare",
-        type=str,
-        required=False,
         default="amd64",
     )
     compare_annotations_parser.add_argument(
         "--compare-repository",
         help="Compare repository name",
-        type=str,
         required=True,
     )
     compare_annotations_parser.add_argument(
         "--annotation",
         help="Annotation to compare",
-        type=str,
-        required=False,
         default="org.opencontainers.image.created",
     )
     compare_annotations_parser.add_argument(
         "--mode",
         help="Annotation to compare",
-        type=str,
-        required=False,
         default="eq",
         choices=["eq", "lt", "gt"],
     )
     compare_annotations_parser.add_argument(
         "--compare-namespace",
         help="Compare registry Namespace",
-        type=str,
-        required=False,
         default="library",
     )
     compare_annotations_parser.add_argument(
         "--compare-reg-domain",
         help="Compare registry domain",
-        type=str,
-        required=False,
         default="registry-1.docker.io",
     )
     compare_annotations_parser.add_argument(
         "--compare-reg-auth-domain",
         help="Compare registry authentication domain",
-        type=str,
-        required=False,
         default="auth.docker.io",
     )
     compare_annotations_parser.add_argument(
         "--compare-reg-auth-service",
         help="Compare registry authentication service",
-        type=str,
-        required=False,
         default="registry.docker.io",
     )
     compare_annotations_parser.add_argument(
         "--compare-user",
         help="User for the compare registry login",
-        type=str,
-        required=False,
         default=None,
     )
     compare_annotations_parser.add_argument(
         "--compare-password",
         help="Password for the compare registry login",
-        type=str,
-        required=False,
         default=None,
     )
 
