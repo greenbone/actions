2bad493fd9e8932dea3d299aefe6845c2ec527fe
Robert Schardt <robert.schardt@greenbone.net>
2025-09-16 10:35:50 +0200

    Fix: Rewrite test case

--- detect-hidden-unicode/src/main.py
+++ detect-hidden-unicode/src/main.py
@@ -8,15 +8,16 @@ from typing import Optional, Sequence
 import shtab
 
 
-def parse_args(args: Optional[Sequence[str]] = None) -> Namespace:
+def parse_args() -> Namespace:
     parser = ArgumentParser()
     shtab.add_argument_to(parser)
 
     parser.add_argument("--some-arg", help="Something")
 
-    return parser.parse_args(args)
+    return parser.parse_args()
 
 def main():
-    parse_args
+    args = parse_args()
+
 if __name__ == "__main__":
     main()
