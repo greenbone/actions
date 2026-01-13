5504ec59d2c63a45b706da86a96bd46874c298db
Robert Schardt <robert.schardt@greenbone.net>
2026-01-13 11:05:22 +0100

    Fix: Unit-tests

--- detect-hidden-unicode/tests/test_main.py
+++ detect-hidden-unicode/tests/test_main.py
@@ -8,48 +8,49 @@ from src.main import parse_args, get_changed_files_and_apply_filter, print_marke
 
 class ParseArgsTestCase(unittest.TestCase):
     def test_parse_args(self):
-        args = parse_args(["foo", "-s", "--filter", ".*urgs.*"])
+        args = parse_args([".", "--hide_scan_details", "--pr_comment_level", "DEBUG", "--filter", ".*urgs.*"])
 
-        self.assertEqual(args.repopath, "foo")
+        self.assertEqual(args.repopath, ".")
         self.assertEqual(args.filter, ".*urgs.*")
-        self.assertEqual(args.silent, True)
+        self.assertEqual(args.hide_scan_details, True)
+        self.assertEqual(args.pr_comment_level, "DEBUG")
 
 class GetChangedFilesAndApplyFilterTestCase(unittest.TestCase):
-    def test_get_changed_files_and_apply_filter(self):
-        args = parse_args([".", "--filter", ".*detect-hidden-unicode.*"])
-        changed_files = get_changed_files_and_apply_filter(args, "8897bd5e42ee2ca11ce2c939005563309a26278e", "3ec149b4224139e5a98736aa4c6e592e7a21e3c4")
+   def test_get_changed_files_and_apply_filter(self):
+       args = parse_args([".", "--filter", ".*detect-hidden-unicode.*"])
+       changed_files = get_changed_files_and_apply_filter(args, "8897bd5e42ee2ca11ce2c939005563309a26278e", "3ec149b4224139e5a98736aa4c6e592e7a21e3c4")
 
-        self.assertEqual(len(changed_files), 3)
+       self.assertEqual(len(changed_files), 3)
 
 class PrintMarkerTestCase(unittest.TestCase):
-    def test_print_marker(self):
-        detected_markers = 0
-        detected_markers = print_marker(False, "description", 1, 1, "fakePath", detected_markers)
+   def test_print_marker(self):
+       detected_markers = 0
+       detected_markers = print_marker([], "WARNING", False, "description", 1, 1, "fakePath", detected_markers)
 
-        self.assertEqual(detected_markers, 1)
+       self.assertEqual(detected_markers, 1)
 
 class ScanFileTestCase(unittest.TestCase):
-    def test_scan_file(self):
-        file_path = "tests/509_pillar-security_example"
-        detected_markers = 0
-        detected_markers = scan_file(False, file_path)
+   def test_scan_file(self):
+       file_path = "tests/509_pillar-security_example"
+       detected_markers = 0
+       detected_markers = scan_file([], "WARNING", False, file_path)
 
-        self.assertEqual(detected_markers, 509)
+       self.assertEqual(detected_markers, 509)
 
 class ScanMultipleChangedFilesTestCase(unittest.TestCase):
-    def test_scan_multiple_changed_files(self):
-        detected_markers = scan_multiple_changed_files(True, ["tests/509_pillar-security_example", "tests/12_basic_example" ])
+   def test_scan_multiple_changed_files(self):
+       detected_markers = scan_multiple_changed_files([], "WARNING", True, ["tests/509_pillar-security_example", "tests/12_basic_example" ])
 
-        self.assertEqual(detected_markers, 521)
+       self.assertEqual(detected_markers, 521)
 
 class ScanSingleChangedFileTestCase(unittest.TestCase):
-    def test_scan_single_changed_file(self):
-        detected_markers = scan_single_changed_file(True, "tests/12_basic_example")
+   def test_scan_single_changed_file(self):
+       detected_markers = scan_single_changed_file([], "WARNING", True, "tests/12_basic_example")
 
-        self.assertEqual(detected_markers, 12)
+       self.assertEqual(detected_markers, 12)
 
 class ScanChangedFilesTestCase(unittest.TestCase):
     def test_ChangedFilesTestCase(self):
-        detected_markers = scan_changed_files(True, ["tests/509_pillar-security_example", "tests/12_basic_example" ])
+        detected_markers = scan_changed_files([], "WARNING", True, ["tests/509_pillar-security_example", "tests/12_basic_example" ])
 
         self.assertEqual(detected_markers, 521)
