71f604794bf2f3e684214d07c6c90c72aeb57bcb
Björn Ricks <bjoern.ricks@greenbone.net>
2026-09-21 13:08:41 +0200

    Change: Use httpx2 instead of httpx
    Pontos will use httpx2 instead of httpx.

--- conventional-commits/action/commits.py
+++ conventional-commits/action/commits.py
@@ -24,7 +24,7 @@ from json import JSONDecodeError
 from pathlib import Path
 from typing import NoReturn
 
-import httpx
+from httpx2 import HTTPStatusError
 from pontos.changelog.conventional_commits import ConventionalCommits
 from pontos.github.actions import Console, GitHubEvent
 from pontos.github.api import GitHubAsyncRESTApi
@@ -137,7 +137,7 @@ class Commits:
                         cc_report_comment,
                         "\n".join(comment_lines),
                     )
-            except httpx.HTTPStatusError as e:
+            except HTTPStatusError as e:
                 try:
                     json = e.response.json()
                     message = json.get("message")
